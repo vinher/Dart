8e36e50e4e2d613781ac7dc4feda69db0c61963b
Kevs <programadorweb.jr@skytex.com.mx>
2023-10-04 08:31:54 -0600

    peticiones

--- paquetes_dart/lib/paquetes_dart.dart
+++ paquetes_dart/lib/paquetes_dart.dart
@@ -1,3 +1,12 @@
-int calculate() {
-  return 6 * 7;
+import 'package:http/http.dart' as http;
+import 'package:paquetes_dart/classes/reqres_response.dart';
+
+void main(List<String> arguments) async {
+  final url = Uri.parse('https://reqres.in/api/users?page=2');
+  http.get(url).then((res) {
+    final resp = responseDataFromJson(res.body);
+    //Desestrtuctures Objetos
+    print(resp.data[0].email);
+    print(resp.page);
+  });
 }
